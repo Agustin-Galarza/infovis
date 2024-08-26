2d4c893cdee266a3116a4c0a7f9dc0a7eb0ec0a1
Agustin Galarza <agalarza@itba.edu.ar>
2024-08-26 12:30:51 -0300

    Updated graph

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,28 @@ function App() {
   return (
     <div className="dark flex h-screen w-full flex-col items-center gap-4 bg-background text-foreground">
       <div className="flex items-center justify-center gap-4 p-4">
-        <span className="font-bold text-red-400">
-          ab<span className="font-bold text-neutral-200">enve</span>
+        <span className="font-bold">
+          agalarza
         </span>
 
         <img
           className="inline-block h-8 w-8 border-spacing-8 rounded-full border border-border"
-          src="./images/ab-square.png"
-          alt="abenve"
+          src="./images/a.webp"
+          alt="agalarza"
         />
       </div>
-      <div className="flex max-w-[30rem] flex-col items-center gap-2">
+      <div className="flex max-w-[55rem] flex-col items-center gap-2">
         <img
-          src="./charts/arc_diagram.svg"
-          alt="arc diagram"
-          className="w-[30rem] rounded-md"
+          src="./charts/rawgraph.svg"
+          alt="rawgraph diagram"
+          className="w-[55rem] rounded-md"
         />
         <div className="flex w-full flex-col gap-1">
           <span className="text-base text-foreground">
-            Graph showing the relationship between the number of male and female
-            students in each plan.
+            Graph showing the relationship between budget and box office earning, grouped by genre, for the higest grossing movies in history.
           </span>
           <small className="text-sm text-muted-foreground">
-            19 / 8 - Create from{' '}
+            26 / 8 - Create from{' '}
             <a
               target="_blank"
               className="text-green-400 hover:cursor-pointer hover:underline"
